77e5b8edce015585d0f28fb7bcd0ae4ef6c010f6
jax <kennethpsinclair@yahoo.com>
2021-08-31 14:08:16 -0400

    Working oon fresh nav bar1 healthy

--- src/App.js
+++ src/App.js
@@ -2,65 +2,73 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Alert, Breadcrumb, Container, Row, Col, Card, Form, Navbar  } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Nav from './nav';
+import Bar from './nav';
 import Prints from './prints';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <img src={logo} width={128} height={77} className="App-logo" alt="logo" />
-      <header className="App-header">
-        <Container fluid>
-        <Card className="mt-3 Card" style={{color: "#000"}}>
-          <Card.Img src="https://picsum.photos/300/100"/>
-          <Card.Body>
-            <Card.Title>
-              Card Example
-            </Card.Title>
-            <Card.Text>
-              This is an example of a react bootstrap cards
-            </Card.Text>
-            <Button variant="primary">Read More</Button>
-          </Card.Body>
-        </Card>
-          <Row>
-            <Col md>
-            <p className="Paragraph"> Welcome to Hatchet Press, My name is Kenneth Sinclair and I'm a Writer, Printmaker, and Musican looking to interacte with and grow a authintic community.</p>
-            </Col>
-            <Col md>
-            <p className="Paragraph"> Hey is this how you make a second coloumn</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            <Form>
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Example@email.com" />
-                <Form.Text className="text-muted">
-                  Well never share youre email address, trust us!
-                  </Form.Text>
-                </Form.Group>
-              </Form>
-            </Col>
-            <Col>
-            <Form>
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="secondary" type="submit">Login</Button>
-              </Form>
-            </Col>
-          </Row>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </Container>
-      </header>
+    <Bar />
+    <Route path="/" exact component={Home}/>
+    <Route path="/prints" component={Prints}/>
     </div>
+    </Router>
   );
 }
+
+const Home = () =>(
+  <div>
+  <header className="App-header">
+  <Card className="Card" style={{color: "#000"}}>
+    <Card.Img src="https://picsum.photos/300/50"/>
+    <Card.Body>
+      <Card.Title>
+        Card Example
+      </Card.Title>
+      <Card.Text>
+        This is an example of a react bootstrap cards
+      </Card.Text>
+      <Button variant="primary">Read More</Button>
+    </Card.Body>
+  </Card>
+  <Row className='Row'>
+    <Col md>
+    <p className="Paragraph"> Hey check it out Paragraph #1 awaits your input. </p>
+    </Col>
+    <Col md>
+    <p className="Paragraph"> Hey is this how you make a second Paragraph?</p>
+    </Col>
+  </Row>
+  <Row >
+    <Col md>
+    <Form className='forms' md>
+      <Form.Group controlId="formEmail">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" placeholder="Example@email.com" />
+        <Form.Text className="text-muted">
+          Well never share youre email address, trust us!
+        </Form.Text>
+      </Form.Group>
+    </Form>
+  </Col>
+  <Col md>
+  <Form className='forms' md>
+    <Form.Group controlId="formPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button href="./About" className="Button" variant="primary" type="submit">Login</Button>
+    </Form>
+  </Col>
+  </Row>
+  <a className="App-link" href="./About" target="_blank" rel="noopener noreferrer"
+  >
+  Take a look inside
+  </a>
+</header>
+  </div>
+);
 
 export default App;
--- src/nav.js
+++ src/nav.js
@@ -1,17 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Link } from "react-router-dom";
+import {Navbar,Nav,NavDropdown,Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function Nav() {
+function Bar() {
+
   return (
-    <nav>
-      <h3>Logo</h3>
-      <ul>
-        <li>Prints</li>
-      </ul>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="/">Peak</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+
+          <Nav.Link as={Link} to={"./prints"}>Prints</Nav.Link>
+          <Nav.Link as={Link} to={"./"}>Home</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Quests</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">The Vibe</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something to do</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+      </Navbar>
   );
 }
 
-export default Nav;
+export default Bar;
