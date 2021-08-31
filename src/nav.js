76ca1db2e4d7eeac47a55e5e518ee9e0ebe71d04
jax <kennethpsinclair@yahoo.com>
2021-08-31 13:09:32 -0400

    fresh for router

---
+++ src/nav.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Nav() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>Prints</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/prints.js
+++ src/prints.js
@@ -2,27 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Alert, Breadcrumb, Container, Row, Col, Card, Form, Navbar  } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Nav from './nav';
-import Prints from './prints';
 
-function App() {
+function Prints() {
   return (
     <div className="App">
       <img src={logo} width={128} height={77} className="App-logo" alt="logo" />
       <header className="App-header">
         <Container fluid>
-        <Card className="mt-3 Card" style={{color: "#000"}}>
-          <Card.Img src="https://picsum.photos/300/100"/>
-          <Card.Body>
-            <Card.Title>
-              Card Example
-            </Card.Title>
-            <Card.Text>
-              This is an example of a react bootstrap cards
-            </Card.Text>
-            <Button variant="primary">Read More</Button>
-          </Card.Body>
-        </Card>
+
           <Row>
             <Col md>
             <p className="Paragraph"> Welcome to Hatchet Press, My name is Kenneth Sinclair and I'm a Writer, Printmaker, and Musican looking to interacte with and grow a authintic community.</p>
@@ -63,4 +50,4 @@ function App() {
   );
 }
 
-export default App;
+export default Prints;
