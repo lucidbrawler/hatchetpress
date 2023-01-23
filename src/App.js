3a5fc7da3814c1f4c09f852d0733bfba71379448
lucidbrawler <kennethpsinclair@yahoo.com>
2023-01-22 20:32:57 -0500

    added variables for event dates to keep formating and updating that easier

--- src/App.js
+++ src/App.js
@@ -35,16 +35,34 @@ function App() {
     </Router>
   );
 }
-const event1 = "Sunset Stroll an arts n crafts Roller Social!"
+const event1 = "TBA"
+/*
+EVENT CALANDER AND DATES
+
 const link1 = "https://www.instagram.com/p/ChBJqFPuYP7/"
+const eventdate1 = "sep 4th: 6pm-9pm"
+const event1description = "DJ phantom and vendor market"
+const event1address = "@ 509 INNER HARBOR, Syracuse, NY"
+
 const event2 = "Ithaca Night Bazar!"
 const link2 = "https://www.eventbrite.com/e/ithaca-night-bazaar-tickets-302363706987"
+const eventdate2 = "sept 15th: 6pm-9pm"
+const event2description = "Live Music, Burlesqe show, and Vendor Market"
+const event2address = "@ Steamboat Landing , Ithaca, NY"
+
+
 const event3 = "918.22 PRINT PARTY"
 const link3 = "https://www.instagram.com/p/ChSlyQJOkAK/"
+const eventdate3 = "sep 16th: 2pm-6pm"
+const event3description = "2 days of printmaking partying with amazing artists, various demonstrations, merch, raffles and more."
+const event3address = "@ 509 w Fayette St, Syracuse, NY"
+
 const event4 = "A Trip To The Moon Art Market!"
 const link4 = "https://www.instagram.com/p/ChR4jhxOLAN/"
-
-
+const eventdate4 = "oct 8th: 2pm-7pm"
+const event4description = "50+ Local artisan vendor's and Live Music!"
+const event4address = "@ 100 college ave , Syracuse, NY"
+*/
 
 const Home = () => (
 
@@ -66,23 +84,25 @@ const Home = () => (
                 </p><p>
                 <br/><br/>
                 <p className='Paragraph3'>UPCOMING EVENTS:<br/><br/>
-                <p> <strong className='event'>sep 4th: 6pm-9pm</strong>
+                <h1>{event1}</h1>
+                {/* 
+                <p> <strong className='event'>{eventdate1}</strong>
                  <h2><a className='event' href ={link1}>{event1}</a>
-                 </h2> DJ phantom and vendor market
-                 <br/> @ 509 INNER HARBOR, Syracuse, NY</p><p> 
-                   <strong className='event'>sept 15th: 6pm-9pm</strong>
+                 </h2> {event1description}
+                 <br/>{event1address} </p><p> 
+                   <strong className='event'>{eventdate2}</strong>
                  <h2><a className='event' href={link2}>{event2}</a>
-                 </h2> Live Music, Burlesqe show, and Vendor Market<br/> @ Steamboat Landing , Ithaca, NY</p>
+                 </h2> {event2description}<br/>{event2address} </p>
                  <br/><br/>
                 
-                <p> <strong className='event'>sep 16th: 2pm-6pm<br></br>sept 17th: 12pm-4pm</strong>
+                <p> <strong className='event'>{eventdate3}<br></br>sept 17th: 12pm-4pm</strong>
                  <h2><a className='event' href ={link3}>{event3}</a>
-                 </h2> 2 days of printmaking partying with amazing artists, various demonstrations, merch, raffles and more.  
-                 <br/> @ 509 w Fayette St, Syracuse, NY</p><p> <
-                   strong className='event'>oct 8th: 2pm-7pm</strong>
+                 </h2>{event3description} 
+                 <br/> {event3address}</p><p> 
+                   <strong className='event'>{eventdate4}</strong>
                  <h2><a className='event' href={link4}>{event4}</a>
-                 </h2> 50+ Local artisan vendor's and Live Music!<br/> @ 100 college ave , Syracuse, NY</p>
-                 
+                 </h2>{event4description}<br/> {event4address}</p>
+                 */}
                  </p>
                 
           
