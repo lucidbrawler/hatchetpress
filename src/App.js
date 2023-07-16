3f94a6beadc0ec7cfd4265a74604e98a09cf26c1
lucidbrawler <kennethpsinclair@yahoo.com>
2023-07-16 10:30:10 -0400

    updating event list for fall 2023

--- src/App.js
+++ src/App.js
@@ -30,35 +30,35 @@ function App() {
   );
 }
 // EVENT CALANDER AND DATES
-const event1 = "The Fool's Variety Show"
+const event1 = "Art Garden and sustainability trail"
+const link1 = "https://www.instagram.com/p/CttUcaZuAnf/"
+const eventdate1 = "August 5th 9:30am-2:30pm"
+const event1description = "Tour unique gardens with sustainable elements- local artists' work -free eco zone sourced native plant for attendees!"
+const event1address =  "@ 1438  Comstock Ave, Syracuse, NY, United States, New York  13210"
+const event1addresslink="https://www.google.com/maps/place/1438+Comstock+Ave,+Syracuse,+NY+13210/@43.0180214,-76.1284846,17z/data=!4m15!1m8!3m7!1s0x89d9f48106709779:0x1d726067131ee634!2s1438+Comstock+Ave,+Syracuse,+NY+13210!3b1!8m2!3d43.0180214!4d-76.1284846!16s%2Fg%2F11c26rfllt!3m5!1s0x89d9f48106709779:0x1d726067131ee634!8m2!3d43.0180214!4d-76.1284846!16s%2Fg%2F11c26rfllt?entry=ttu"
 
-
-
-const link1 = "https://www.instagram.com/p/Cpn-1TUOnNl/"
-const eventdate1 = "April 1st: 7pm-11pm"
-const event1description = "Local Artist Showcase, Performances, and More"
-const event1address =  "@ 200 S Geddes St, Syracuse, NY, United States, New York"
-const event1addresslink="https://www.google.com/maps/place/The+Little+Room/@43.0456975,-76.1713244,15z/data=!4m2!3m1!1s0x0:0x20d259a020af6c54?sa=X&ved=2ahUKEwii4dHCq9f9AhV4E1kFHdgoB1oQ_BJ6BAhAEAg"
-
-const event2 = "Sip and Shop!"
-
-const eventdate2 = "April 16th: 11am-3pm"/*
+const event2 = "Wet Hot American Market II: Summer Camp!"
 const link2 = "https://www.eventbrite.com/e/ithaca-night-bazaar-tickets-302363706987"
+const eventdate2 = "Aug 6th: TBD"
 const event2description = "variety of goods and art from local vendors"
-const event2address = "@ Alto Cinco Syracuse, Ny"
+const event2address = "@ Sharkey’s Events Center Liverpool NY 13090"
+const event2addresslink="https://www.google.com/maps/place/Sharkey's+Banquets+%26+Events/@43.1218637,-76.2134163,15z/data=!4m6!3m5!1s0x89d9ee3754e1b16b:0x5e03f2f639165ba1!8m2!3d43.1218637!4d-76.2134163!16s%2Fg%2F11h0lc7bp?entry=ttu"
 
 
-const event3 = "918.22 PRINT PARTY"
+const event3 = " Salt City Book Arts PRINT PARTY"
 const link3 = "https://www.instagram.com/p/ChSlyQJOkAK/"
-const eventdate3 = "sep 16th: 2pm-6pm"
-const event3description = "2 days of printmaking partying with amazing artists, various demonstrations, merch, raffles and more."
-const event3address = "@ 509 w Fayette St, Syracuse, NY"
+const eventdate3 = "Sept 23rd"
+const event3description = "Printmaking Demos! vendors! exhibition!"
+const event3address = "@ Maker Space a downtown Central Library"
+const event3addresslink = "https://www.google.com/maps/place/Onondaga+County+Public+Libraries:+Central+Library/@43.045845,-76.1534929,17.91z/data=!3m1!5s0x89d9f3bee8eb90c9:0x1f0390272e0376e!4m6!3m5!1s0x89d9f3befd4524d3:0x361247b12a88c269!8m2!3d43.0459585!4d-76.1517369!16s%2Fg%2F1vgx1rl2?entry=ttu"
 
+/*
 const event4 = "A Trip To The Moon Art Market!"
 const link4 = "https://www.instagram.com/p/ChR4jhxOLAN/"
 const eventdate4 = "oct 8th: 2pm-7pm"
 const event4description = "50+ Local artisan vendor's and Live Music!"
 const event4address = "@ 100 college ave , Syracuse, NY"
+const event4addresslink = ""
 */
 
 const Home = () => (
@@ -84,23 +84,25 @@ const Home = () => (
                 
                 <br/><br/>
                 <p className='Paragraph3'>UPCOMING EVENTS:<br/><br/>
+                
                 <h1><a className='event' href ={link1}>{event1}</a></h1>
                 {event1description}
                 <p> <strong className='event'>{eventdate1}</strong>
-                 <h2>
-                 </h2> 
                  <a className='event' href={event1addresslink}> {event1address} </a></p>
-                  
-                 <p>   <h2>{event2}</h2></p>
-                 <strong className='event'>{eventdate2}</strong> {/* 
-                 <strong className='event'>{eventdate2}</strong>
-                 {event2description}<br/>{event2address} </p>
+        
+                <h1><a className='event' href={link2}> {event2}</a></h1>
+                   {event2description}
+                   <p> <strong className='event'>{eventdate2}</strong> 
+                    <a className='event' href={event2addresslink} >{event2address} </a></p>
                  <br/><br/>
-                
-                <p> <strong className='event'>{eventdate3}<br></br>sept 17th: 12pm-4pm</strong>
-                 <h2><a className='event' href ={link3}>{event3}</a>
-                 </h2>{event3description} 
-                 <br/> {event3address}</p><p> 
+
+                 <h1><a className='event' href ={link3}>{event3}</a></h1>
+                 {event3description}
+                  <p> <strong className='event'>{eventdate3}</strong>
+                  <a className='event' href={event3addresslink}>{event3address}</a>
+                  </p>
+                 
+                   {/* 
                    <strong className='event'>{eventdate4}</strong>
                  <h2><a className='event' href={link4}>{event4}</a>
                  </h2>{event4description}<br/> {event4address}</p>
